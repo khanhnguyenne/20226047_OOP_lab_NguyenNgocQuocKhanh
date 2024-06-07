baa5275835ac48be7aa2c73ccb7f223ad7128254
khanhnguyenne <khanh.nnq226047@sis.hust.edu.vn>
2024-06-07 19:33:36 +0700

    Add files via upload

---
+++ Lab04/screen/manager/AddItemToStoreScreen.java
@@ -0,0 +1,65 @@
+package hust.soict.dsai.aims.screen.manager;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class AddItemToStoreScreen extends JFrame {
+    protected JTextField tfTitle, tfCategory, tfCost;
+
+    public AddItemToStoreScreen() {
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+
+        cp.add(createNorth(), BorderLayout.NORTH);
+        cp.add(createCenter(), BorderLayout.CENTER);
+
+        setTitle("Add Item to Store");
+        setSize(600, 400);
+        setVisible(true);
+    }
+
+    JPanel createNorth() {
+        JPanel north = new JPanel();
+        north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
+        north.add(createMenuBar());
+        north.add(createHeader());
+        return north;
+    }
+
+    JMenuBar createMenuBar() {
+        JMenu menu = new JMenu("Options");
+
+        JMenu smUpdateStore = new JMenu("Update Store");
+        JMenuItem addBook = new JMenuItem("Add Book");
+        JMenuItem addCD = new JMenuItem("Add CD");
+        JMenuItem addDVD = new JMenuItem("Add DVD");
+
+        smUpdateStore.add(addBook);
+        smUpdateStore.add(addCD);
+        smUpdateStore.add(addDVD);
+
+        menu.add(smUpdateStore);
+        menu.add(new JMenuItem("View Store"));
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(menu);
+        return menuBar;
+    }
+
+    JPanel createHeader() {
+        JPanel header = new JPanel();
+        header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+
+        JLabel title = new JLabel("Add Item to Store");
+        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 30));
+        title.setForeground(Color.BLUE);
+
+        header.add(Box.createRigidArea(new Dimension(10, 10)));
+        header.add(title);
+        header.add(Box.createHorizontalGlue());
+
+        return header;
+    }
+
+    abstract JPanel createCenter();
+}
