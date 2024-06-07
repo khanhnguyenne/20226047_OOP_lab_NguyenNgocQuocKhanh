bae0380a1c412adf1ee013ac8e02364d8195650b
khanhnguyenne <khanh.nnq226047@sis.hust.edu.vn>
2024-06-07 19:34:34 +0700

    Add files via upload

---
+++ Lab05/javafx/PainterController.java
@@ -0,0 +1,23 @@
+package hust.soict.dsai.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class PainterController {
+    @FXML
+    private Pane drawingAreaPane;
+
+    @FXML
+    private void clearButtonPressed() {
+        drawingAreaPane.getChildren().clear();
+    }
+
+    @FXML
+    private void drawingAreaMouseDragged(MouseEvent event) {
+        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+        drawingAreaPane.getChildren().add(newCircle);
+    }
+}
